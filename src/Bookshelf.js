93bfb991c24cce95b6a08a4cc34175a1a4b02965
Sophie Kowalski <skowalski57@gmail.com>
2019-12-21 21:28:19 +0100

    add Book component to Bookshelf

---
+++ src/Bookshelf.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Book from './Book';
+
+class Bookshelf extends React.Component {
+  render() {
+    const { shelf, books } = this.props;
+
+    const booksOnThisShelf = books.filter(book => book.shelf === shelf.key);
+    // console.log(booksOnThisShelf);
+
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{shelf.name}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {booksOnThisShelf.map(book => (
+              <Book
+                key={book.title} // Change to id when using API
+                book={book}
+                // shelf={shelf.key}
+              />
+            ))}
+          </ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Bookshelf;
